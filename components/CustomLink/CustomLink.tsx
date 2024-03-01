2006395dd8781cf38395b69b7ce7caf73ef7cf8b
yohan <yohan@adelios.fr>
2024-03-02 00:18:31 +0100

    style(font): ajout de la font manrope

--- components/CustomLink/CustomLink.tsx
+++ components/CustomLink/CustomLink.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from './CustomLink.module.scss';
 
 interface Props {
 	label: string;
@@ -7,7 +8,11 @@ interface Props {
 }
 
 const CustomLink = ({ label, href }: Props) => {
-	return <Link href={href}>{label} </Link>;
+	return (
+		<Link className={styles.link} href={href}>
+			{label}{' '}
+		</Link>
+	);
 };
 
 export default CustomLink;
